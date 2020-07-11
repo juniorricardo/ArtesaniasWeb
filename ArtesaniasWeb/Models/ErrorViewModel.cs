d883a608846348e31bd21bbc2dbc23ce2ddced6d
juniorricardo <neo.junior.16@gmail.com>
2020-07-10 22:07:53 -0300

    Inicio

---
+++ ArtesaniasWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ArtesaniasWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
